461e61747fd59ff0a760dcd1cec9d0533bd7cdd3
Taylor Ham <mynameisham@gmail.com>
2016-02-05 23:29:16 -0800

    update function for building select boxes, make sure each skill can only be selected in one slot at a time, add comments for clarity

--- scripts/view.js
+++ scripts/view.js
@@ -1,42 +1,51 @@
 'use strict'
 
 $(document).ready(function() {
+  // Since '#dashboard' element is measured with vw units
+  // and vw units don't account for the width of a scrollbar
+  // we'll add an extra class to change '#dashboard's computed
+  // size if a scrollbar is detected.
   if ($('body').height() > $(window).height()) {
     $('#dashboard').addClass('scrollbar');
   }
-  var html = "";
-  var obj = Skills;
-  for (var key in obj) {
-    if (!obj[key].hasOwnProperty('name')) {
-      for (var subKey in obj[key]) {
-        if (!obj[key][subKey].hasOwnProperty('name')) {
-          for (var subSubKey in obj[key][subKey]) {
-            let value = obj[key][subKey][subSubKey].name;
-            if (value !== undefined) {
-              html += "<option value=\"" + value + "\">" + value + "</option>";
-            }
-          }
-        }
-        let value = obj[key][subKey].name;
-        if (value !== undefined) {
-          html += "<option value=\"" + value + "\">" + value + "</option>";
-        }
-      }
-    }
-    let value = obj[key].name;
-    if (value !== undefined) {
-      html += "<option value=\"" + value + "\">" + value + "</option>";
-    }
+
+  // Create an array of <options>, seeded by a placeholder value.
+  var html = ['<option value="" disabled selected hidden>Choose skill...</option>'];
+  for (var key in Skills) {
+    html.push('<option value="' + Skills[key].name + '">' + Skills[key].name + '</option>');
   }
 
-  document.getElementById("skills1").innerHTML = html;
-  document.getElementById("skills3-1").innerHTML = html;
-  document.getElementById("skills3-2").innerHTML = html;
-  document.getElementById("skills4-1").innerHTML = html;
-  document.getElementById("skills4-2").innerHTML = html;
-  document.getElementById("skills4-3").innerHTML = html;
+  // Create an array of each <select>'s ID that will receive the
+  // 'html' variable we've built for our <options>.
+  var skillIDs = ["skills1", "skills3-1", "skills3-2", "skills4-1", "skills4-2", "skills4-3"]
+
+  // Adds the 'html' array of <options> to each <select> element.
+  for (var i = 0; i < skillIDs.length; i++) {
+    $('#' + skillIDs[i]).html(html);
+  }
+
+}); // end $(document).ready()
+
+// Don't allow duplicate skills to be selected in multiple <select>s.
+$('select[name^="skills"]').change(function(){
+
+  // Set everything to enabled to start.
+  $('select[name^="skills"] option').attr('disabled', false);
+
+  // Loop each <select> and set the selected value to disabled in all other <select>s.
+  $('select[name^="skills"]').each(function(){
+    var $this = $(this);
+    $('select[name^="skills"]').not($this).find('option').each(function() {
+      if ($(this).attr('value') == $this.val()) {
+        $(this).attr('disabled', true);
+      }
+    });
+  });
+
 });
 
+// On resizing of a window, we'll check to see if it has a scrollbar
+// and apply the necessary class, or remove it if the scrollbar is gone.
 $(window).resize(function() {
   if ($('body').height() > $(window).height()) {
     $('#dashboard').addClass('scrollbar');
