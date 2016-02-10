ab5de281677c703a55d6cbc372a62d717beda766
Taylor Ham <mynameisham@gmail.com>
2016-02-10 13:21:05 -0800

    add comments to clarify code for adding descriptions to option tooltips and description elements

--- scripts/view.js
+++ scripts/view.js
@@ -4,36 +4,38 @@ $(document).ready(function() {
   // Since '#dashboard' element is measured with vw units
   // and vw units don't account for the width of a scrollbar
   // we'll add an extra class to change '#dashboard's computed
-  // size if a scrollbar is detected.
+  // size if a scrollbar is detected
   if ($('body').height() > $(window).height()) {
     $('#dashboard').addClass('scrollbar');
   }
 
-  // Create an array of <options>, seeded by a placeholder value.
+  // Create an array of <options>, seeded by a placeholder value
   var html = ['<option value="" disabled selected hidden>Choose skill...</option>'];
-  for (var key in Skills) {
+  for (let key in Skills) {
+    // Add skill name to name and value fields, add description to title for tooltips
     html.push('<option value="' + Skills[key].name + '" title="' + Skills[key].desc + '">' + Skills[key].name + '</option>');
   }
 
   // Create an array of each <select>'s ID that will receive the
-  // 'html' variable we've built for our <options>.
+  // 'html' variable we've built for our <options>
   var skillIDs = ["skills1", "skills3-1", "skills3-2", "skills4-1", "skills4-2", "skills4-3"]
 
-  // Adds the 'html' array of <options> to each <select> element.
-  for (var i = 0; i < skillIDs.length; i++) {
+  // Adds the 'html' array of <options> to each <select> element
+  for (let i = 0; i < skillIDs.length; i++) {
     $('#' + skillIDs[i]).html(html);
   }
 
 }); // end $(document).ready()
 
-// Don't allow duplicate skills to be selected in multiple <select>s.
+// Don't allow duplicate skills to be selected in multiple <select>s
 $('select[name^="skills"]').change(function(){
 
-  // Set everything to enabled to start.
+  // Set everything to enabled to start
   $('select[name^="skills"] option').attr('disabled', false);
 
-  // Loop each <select> and set the selected value to disabled in all other <select>s.
+  // Loop each <select> and set the selected value to disabled in all other <select>s
   $('select[name^="skills"]').each(function(){
+    // Set $self to the current iterated <select>
     let $self = $(this);
     $('select[name^="skills"]').not($self).find('option').each(function() {
       if ($(this).attr('value') == $self.val()) {
@@ -41,16 +43,20 @@ $('select[name^="skills"]').change(function(){
       }
     });
   });
+  // Set $self to changed <select>
   let $self = $(this);
+  // Set $selfSelected to currrent selected <option>
   let $selfSelected = $('#' + $self.attr('id') + ' option:selected');
+  // Create html string for the name of the currently selected skill
   let selfName = '<strong>' + $selfSelected.val() + ' - </strong>';
+  // Set string variable for description of skill
   let selfDescription = $selfSelected.attr('title');
-  console.log($self.attr('id'));
+  // Fill matching description <div> with skill name and description
   $('#' + $self.attr('id') + '-description').html(selfName + selfDescription);
 });
 
 // On resizing of the window, we'll check to see if it has a scrollbar
-// and apply the necessary class, or remove it if the scrollbar is gone.
+// and apply the necessary class, or remove it if the scrollbar is gone
 $(window).resize(function() {
   if ($('body').height() > $(window).height()) {
     $('#dashboard').addClass('scrollbar');
